dbc061a4521cb687c98fea273ab1ae7fd45b303c
Nguyen Le Gia Man <Mannguyengiale@gmail.com>
2026-08-21 17:27:27 +0700

    Cai dat hieu chinh contact

--- public/edit.php
+++ public/edit.php
@@ -1,10 +1,39 @@
 <?php
+
 require_once __DIR__ . '/../src/bootstrap.php';
-require_once __DIR__ . '/../src/classes/Contact.php';
 
 use CT275\Labs\Contact;
 
-$contact = new Contact(null);
+$contact = new Contact($PDO);
+
+$id = isset($_REQUEST['id'])
+    ? filter_var($_REQUEST['id'], FILTER_VALIDATE_INT)
+    : false;
+
+if (!$id || !($contact->find($id))) {
+    redirect('/');
+}
+
+$errors = [];
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+
+    $contactData = [
+        'name' => $_POST['name'] ?? '',
+        'phone' => $_POST['phone'] ?? '',
+        'notes' => $_POST['notes'] ?? '',
+    ];
+
+    $errors = $contact->validate($contactData);
+
+    if (empty($errors)) {
+        $contact->fill($contactData);
+        $contact->save();
+
+        redirect('/');
+    }
+}
 
 include_once __DIR__ . '/../src/partials/header.php';
 ?>
@@ -23,14 +52,21 @@ include_once __DIR__ . '/../src/partials/header.php';
     <div class="row">
       <div class="col-12">
 
-        <form method="post" class="col-md-6 offset-md-3">
+        <form method="post" enctype="multipart/form-data" class="col-md-6 offset-md-3">
 
           <input type="hidden" name="id" value="<?= $contact->id ?>">
 
           <!-- Name -->
           <div class="mb-3">
             <label for="name" class="form-label">Name</label>
-            <input type="text" name="name" class="form-control<?= isset($errors['name']) ? ' is-invalid' : '' ?>" maxlen="255" id="name" placeholder="Enter Name" value="<?= html_escape($contact->name) ?>" />
+
+            <input type="text"
+                   name="name"
+                   class="form-control<?= isset($errors['name']) ? ' is-invalid' : '' ?>"
+                   maxlength="255"
+                   id="name"
+                   placeholder="Enter Name"
+                   value="<?= html_escape($contact->name) ?>" />
 
             <?php if (isset($errors['name'])) : ?>
               <span class="invalid-feedback">
@@ -42,7 +78,14 @@ include_once __DIR__ . '/../src/partials/header.php';
           <!-- Phone -->
           <div class="mb-3">
             <label for="phone" class="form-label">Phone Number</label>
-            <input type="text" name="phone" class="form-control<?= isset($errors['phone']) ? ' is-invalid' : '' ?>" maxlen="255" id="phone" placeholder="Enter Phone" value="<?= html_escape($contact->phone) ?>" />
+
+            <input type="text"
+                   name="phone"
+                   class="form-control<?= isset($errors['phone']) ? ' is-invalid' : '' ?>"
+                   maxlength="255"
+                   id="phone"
+                   placeholder="Enter Phone"
+                   value="<?= html_escape($contact->phone) ?>" />
 
             <?php if (isset($errors['phone'])) : ?>
               <span class="invalid-feedback">
@@ -53,8 +96,12 @@ include_once __DIR__ . '/../src/partials/header.php';
 
           <!-- Notes -->
           <div class="mb-3">
-            <label for="notes" class="form-label">Notes </label>
-            <textarea name="notes" id="notes" class="form-control<?= isset($errors['notes']) ? ' is-invalid' : '' ?>" placeholder="Enter notes (maximum character limit: 255)"><?= html_escape($contact->notes) ?></textarea>
+            <label for="notes" class="form-label">Notes</label>
+
+            <textarea name="notes"
+                      id="notes"
+                      class="form-control<?= isset($errors['notes']) ? ' is-invalid' : '' ?>"
+                      placeholder="Enter notes (maximum character limit: 255)"><?= html_escape($contact->notes) ?></textarea>
 
             <?php if (isset($errors['notes'])) : ?>
               <span class="invalid-feedback">
@@ -62,9 +109,11 @@ include_once __DIR__ . '/../src/partials/header.php';
               </span>
             <?php endif ?>
           </div>
-
           <!-- Submit -->
-          <button type="submit" name="submit" class="btn btn-primary">Update Contact</button>
+          <button type="submit" name="submit" class="btn btn-primary">
+            Update Contact
+          </button>
+
         </form>
 
       </div>
